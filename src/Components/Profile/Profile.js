5a89799e1ca70208a0c36ffb7f99ee7bfd63a678
FAlSaleh94 <94alsaleh@gmail.com>
2022-03-21 08:52:58 +0300

    p2

--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -1,8 +1,8 @@
 import "./Profile.css";
 import React from "react";
 import ReactPlayer from "react-player";
-import DropdownButton from "react-bootstrap/DropdownButton";
-import { Dropdown } from "react-bootstrap";
+import { useAccordionButton } from "react-bootstrap/AccordionButton";
+import { Accordion } from "react-bootstrap";
 
 function Profile() {
   return (
@@ -95,37 +95,46 @@ function Profile() {
                           <div className="row">
                             <div className="">
                               <div className="dropdown">
-                                <DropdownButton
-                                  alignRight
-                                  title="Proficiencies"
-                                  id="dropdown-menu-align-right"
-                                >
-                                  <Dropdown.Item eventKey="option-1">
-                                    Crochet
-                                  </Dropdown.Item>
-                                  <Dropdown.Item eventKey="option-2">
-                                    Scuba Diving
-                                  </Dropdown.Item>
-                                  <Dropdown.Item eventKey="option-3">
-                                    Kick Boxing
-                                  </Dropdown.Item>
-                                </DropdownButton>
-
-                                <DropdownButton
-                                  alignRight
-                                  title="Languages"
-                                  id="dropdown-menu-align-right"
-                                >
-                                  <Dropdown.Item eventKey="option-1">
-                                    Arabic
-                                  </Dropdown.Item>
-                                  <Dropdown.Item eventKey="option-2">
-                                    English
-                                  </Dropdown.Item>
-                                  <Dropdown.Item eventKey="option-3">
-                                    Fish
-                                  </Dropdown.Item>
-                                </DropdownButton>
+                                <Accordion defaultActiveKey="0" flush>
+                                  <Accordion.Item eventKey="0">
+                                    <Accordion.Header>
+                                      Accordion Item #1
+                                    </Accordion.Header>
+                                    <Accordion.Body>
+                                      Lorem ipsum dolor sit amet, consectetur
+                                      adipiscing elit, sed do eiusmod tempor
+                                      incididunt ut labore et dolore magna
+                                      aliqua. Ut enim ad minim veniam, quis
+                                      nostrud exercitation ullamco laboris nisi
+                                      ut aliquip ex ea commodo consequat. Duis
+                                      aute irure dolor in reprehenderit in
+                                      voluptate velit esse cillum dolore eu
+                                      fugiat nulla pariatur. Excepteur sint
+                                      occaecat cupidatat non proident, sunt in
+                                      culpa qui officia deserunt mollit anim id
+                                      est laborum.
+                                    </Accordion.Body>
+                                  </Accordion.Item>
+                                  <Accordion.Item eventKey="1">
+                                    <Accordion.Header>
+                                      Accordion Item #2
+                                    </Accordion.Header>
+                                    <Accordion.Body>
+                                      Lorem ipsum dolor sit amet, consectetur
+                                      adipiscing elit, sed do eiusmod tempor
+                                      incididunt ut labore et dolore magna
+                                      aliqua. Ut enim ad minim veniam, quis
+                                      nostrud exercitation ullamco laboris nisi
+                                      ut aliquip ex ea commodo consequat. Duis
+                                      aute irure dolor in reprehenderit in
+                                      voluptate velit esse cillum dolore eu
+                                      fugiat nulla pariatur. Excepteur sint
+                                      occaecat cupidatat non proident, sunt in
+                                      culpa qui officia deserunt mollit anim id
+                                      est laborum.
+                                    </Accordion.Body>
+                                  </Accordion.Item>
+                                </Accordion>
                               </div>
                             </div>
 
