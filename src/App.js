4d1e2479646a8068d0215fdc710d5b54cfa45494
ripleyhyde <you@example.com>
2022-03-22 10:34:47 +0300

    working on search bar

--- src/App.js
+++ src/App.js
@@ -2,18 +2,18 @@ import { Fragment } from "react";
 import "./App.css";
 import NavBar from "./Components/NavBar/NavBar";
 import Profile from "./Components/Profile/Profile";
+import SearchBar from "./Components/SearchBar";
+import mentors from "../src/MentorsDummyData.json";
 
 function App() {
   return (
     <>
-      <div>
-        <h1>SIFU</h1>
+      {/* <h1>SIFU</h1>
       </div>
-
       <NavBar />
       <div>
-        <Profile />
-      </div>
+        <Profile /> */}
+      <SearchBar />
     </>
   );
 }
